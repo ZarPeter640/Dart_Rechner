20187ead5aaef5fca91b73359cfe3f8eca0553f0
Jonas Peter <jonas.peter@bkk-mobil-oil.de>
2021-08-10 14:21:56 +0200

    Add project files.

---
+++ Dart_Rechner/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Dart_Rechner
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private String _buttonText;
+
+        public string ButtonText
+        {
+            get { return _buttonText; }
+            set { 
+                SetProperty<string>(ref _buttonText, value, nameof(ButtonText));
+            }
+        }
+        //public ICommand StartCalculatorCommand { get; }
+
+        
+
+        private DelegateCommand _startCalculatorCommand;
+
+        public DelegateCommand StartCalculatorCommand
+        {
+            get
+            {
+                return _startCalculatorCommand ??= new DelegateCommand(StartCalculator);
+            }
+        }
+
+        public MainViewModel()
+        {
+            //_startCalculator = new DelegateCommand(StartCalculator);
+            _buttonText = "Hallo";
+        }
+
+        private void StartCalculator(object commandParameter)
+        {
+            ButtonText = "Huhu";
+        }
+    }
+}
